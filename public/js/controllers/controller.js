6e47d980993e52044caf7f098fef658748b006db
Sara Wong <swong2@wellesley.edu>
2015-01-20 11:43:01 -0800

    browsing functional

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -1,8 +1,13 @@
 App.controller('appController', function($scope, Storage, $location, sharedProperties, $upload){
     $scope.user = sharedProperties.getProperty();
     $scope.login = {};
-	$scope.browse = sharedProperties.getAll();	   
-    // BEGIN VALIDATIONS FOR LOGIN --->                                 
+	$scope.browse = sharedProperties.getAll();	
+    if((typeof $scope.user.chosen !== "undefined") && ($scope.user.chosen.length !== 0)){ 
+        Storage.getAllChosen($scope.user.chosen, function(data){
+            $scope.hotties = data;
+        });                                                                         // pass in an array of id strings, and retrieve the people objects from the array.
+    }
+    // BEGIN VALIDATIONS FOR LOGIN --->   
     $scope.login.errors = {};
     $scope.loginClicked = function(){                                               // login validations BEGIN ------>
         $scope.login.errors = {};                                                   // reset any prior errors
