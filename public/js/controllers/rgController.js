6458e82c2a100b4b37e6bdd41c23362e9b168fbc
Eric Chen <mouthwood@gmail.com>
2015-01-20 13:35:46 -0800

    complete register section

--- public/js/controllers/rgController.js
+++ public/js/controllers/rgController.js
@@ -1,34 +1,119 @@
 App.controller('registerCtrl', function($scope, $location, registerFty) {
   
-  $scope.getStarted = function() {
-    registerFty.addNewUser();
-  };
+    var interest = {
+        singing: false,
+        crying: false,
+        biking: false,
+        assassine: false,
+        firefly: false,
+        eating: false
+    };
 
-  $scope.addGender = function() {
-    var gender=$('input[type="radio"]:checked').val();
-      if(gender===undefined) {
-        alert("Your gender is very important to us!!");
-      } else {
-        registerFty.addGender(gender);
-        $location.path('/seeking');
-      } 
-  };
+    $scope.getStarted = function() {
+        registerFty.addNewUser();
+    };
 
-  $scope.addSeeking = function() {
-    var genderS=$('input[type="radio"]:checked').val();
-    console.log('seeking '+genderS);
-      if(genderS===undefined) {
-        alert("What you are looking for is very important to us!!");
-      } else {
-        registerFty.addSeeking(genderS);
-        $location.path('/interest');
-      } 
-  };
+    $scope.addGender = function() {
+        var gender=$('input[type="radio"]:checked').val();
+        if(gender===undefined) {
+            alert("Your gender is very important to us!!");
+        } else {
+            registerFty.addGender(gender);
+            $location.path('/seeking');
+        } 
+    };
 
-  
+    $scope.addSeeking = function() {
+        var genderS=$('input[type="radio"]:checked').val();
+        if(genderS===undefined) {
+            alert("What you are looking for is very important to us!!");
+        } else {
+            registerFty.addSeeking(genderS);
+            $location.path('/birthday');
+        } 
+    };
+
+    $scope.addBirthday = function() {
+        if($scope.bday===undefined) {
+            alert("Birthday please!! We want to wish you happy birthday each year!");
+        } else {
+            registerFty.addBirthday($scope.bday);
+            $location.path('/interest');
+        } 
+    }
 
 
 
+    //JQuery for Interests page
+    $('a.singing').on('click', function() {
+        $(this).children('span').toggleClass('green'); 
+        if(interest.singing === true){
+            interest.singing = false;
+        } else {
+            interest.singing = true;
+        }
+    });
 
+    $('a.crying').on('click', function() {
+        $(this).children('span').toggleClass('green');
+        if(interest.crying === true){
+            interest.crying = false;
+        } else {
+            interest.crying = true;
+        }
+    });
 
+    $('a.biking').on('click', function() {
+        $(this).children('span').toggleClass('green');
+        if(interest.biking === true){
+            interest.biking = false;
+        } else {
+            interest.biking = true;
+        }
+    });
+
+    $('a.assassine').on('click', function() {
+        $(this).children('span').toggleClass('green');
+        if(interest.assassine === true){
+            interest.assassine = false;
+        } else {
+            interest.assassine = true;
+        }
+    });
+
+    $('a.firefly').on('click', function() {
+        $(this).children('span').toggleClass('green');
+        if(interest.firefly === true){
+            interest.firefly = false;
+        } else {
+            interest.firefly = true;
+        }
+
+    });
+
+    $('a.eating').on('click', function() {
+        $(this).children('span').toggleClass('green');
+        if(interest.eating === true){
+            interest.eating = false;
+        } else {
+            interest.eating = true;
+        }
+    });
+
+    
+    $scope.addInterests = function() {
+        registerFty.addInterests(interest);
+        $location.path('/register');
+    }
+
+    $scope.turnIn = function() {
+        if($scope.reg === undefined){
+            alert("Username and password cannot be blank");
+        } else if($scope.reg.name === undefined || $scope.reg.password === undefined) {
+            alert("Username or password cannot be blank");
+        } else {
+            registerFty.submit($scope.reg);
+            $location.path('/login');
+        }
+    }
 })
--- public/js/factories/rgFactory.js
+++ public/js/factories/rgFactory.js
@@ -1,32 +1,47 @@
 App.factory('registerFty', function() {
-    var user = {
-        photo:    null,
-        username:   null,
-        password:   null,
-        gender:   null,
-        seeking:  null,
-        img:    { 
-                data:       null, 
-                contentType:  null
-              },
-        birthday:   null,
-        interests:  { type: Array, default : [] },
-        aboutme:  null,
-        chosen:   { type: Array, default : [] }
-    }
-    
-    var newUser = {};
-    
-    return {
-      addNewUser: function() {
-        newUser = user;
-      },
-      addGender: function(gender) {
-        newUser.gender = gender;
-      },
-      addSeeking: function(seeking) {
-        newUser.seeking = seeking;
-        console.log(newUser);
-      }
-    }
+	var user = {
+		photo:    '',
+		username:   null,
+		password:   null,
+		gender:   null,
+		seeking:  null,
+		img:    { 
+				data:       null, 
+				contentType:  null
+			  },
+		birthday:   null,
+		interests: [],
+		aboutme:  'Tell me a little more about yourself. <3',
+		chosen:   []
+	}
+	
+	var newUser = {};
+	
+	return {
+		addNewUser: function() {
+			newUser = user;
+		},
+		addGender: function(gender) {
+			newUser.gender = gender;
+		},
+		addSeeking: function(seeking) {
+			newUser.seeking = seeking;
+		},
+		addInterests: function(interests) {
+			newUser.interests = [];
+			for(var interest in interests) {
+				if(interests[interest]===true)
+					newUser.interests.push(interest);
+			}
+			console.log(newUser.interests);
+		},
+		addBirthday: function(bday) {
+			newUser.birthday = bday;
+		},
+		submit: function(register) {
+			newUser.username = register.name;
+			newUser.password = register.password;
+			console.log(newUser);
+		}
+	}
 })
